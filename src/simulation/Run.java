bde3af08f59589bf6498cf3762fed04c87527d48
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2019-04-27 11:25:34 +0100

    Run commented

--- src/simulation/Run.java
+++ src/simulation/Run.java
@@ -9,8 +9,20 @@ import org.w3c.dom.Document;
 
 import parsing.SimulationParser;
 
+/**
+ * Run.java
+ * This is the main class.
+ *
+ * @author John Mendonça, Manuel Domingues, Rúben Gomes
+ * @since 04-26-2019
+ */
+
 public class Run {
 
+	/**
+    	* Verifies if the xml is given as an input argument. Begins the parsing of the xml and the simulation.
+   	*/
+	
 	public static void main(String[] args) {
 			
 		if(args.length == 0)
